09bcd1375ab4e3ee83a3672626acc01c264d49fc
saqibmanan <saqibmanan6@gmail.com>
2025-06-27 22:20:36 +0530

    import time lib

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,7 +3,7 @@
 import os
 from dotenv import load_dotenv
 from typing import Union
-
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
