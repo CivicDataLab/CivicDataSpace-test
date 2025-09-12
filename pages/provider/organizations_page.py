940bb2e6a0c25ce54abc9dc5d44e12d1ba39314c
saqibmanan <saqibmanan6@gmail.com>
2025-09-12 12:02:07 +0530

    updated code for the org flow, create dataset

--- pages/provider/organizations_page.py
+++ pages/provider/organizations_page.py
@@ -2,9 +2,18 @@
 
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
+from locators.provider.org_locators import OrgLocators
 
 class OrganizationsPage(BasePage):
     """POM for the Organizations list under /dashboard."""
 
     def is_loaded(self) -> bool:
         return self.find((By.XPATH, "//h1[text()='Organizations']")).is_displayed()
+
+    def select_org(self) -> bool:
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, ORGLocators.ORG_TEST)),
+            message="Timed out waiting for the 'ORG Dashboard' card to be clickable"
+        ).click()
+        return self
