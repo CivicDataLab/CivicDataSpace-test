953bf4a373aa83f397dca9698fb134636527f521
Saqib <saqibmanan6@gmail.com>
2026-06-04 18:21:10 +0530

    fix missing top-level By import in base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 # pages/base_page.py
 import platform
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
